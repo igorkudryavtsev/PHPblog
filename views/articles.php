f862e46bae7a353c544b69440b0ad4ddd7f0e642
IgorRock <scorp1000@mail.ru>
2016-11-27 01:02:45 +0300

    PHPblog

---
+++ views/articles.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<hmtl>
+    <head>
+        <meta charset="utf-8">
+        <title>Блог</title>
+        <link rel="stylesheet" href="style.css">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <!-- Header (navbar) -->
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a id="blog" class="navbar-brand" href="index.php">Блог</a>
+                    </div>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="admin">Панель администратора</a></li>
+                    </ul>
+                </div>
+            </nav> 
+            <!-- END Header (navbar) -->
+            <?php foreach($articles as $article): ?>
+            <div class="article">
+                <h3><a href="article.php?id=<?=$article['id']?>"><?=$article['title']?></a></h3>
+                <em>Опубликованно: <?=$article['date']?></em>
+                <p><?=articles_intro($article['content'])?></p>
+            </div>
+            <?php endforeach ?>
+            <footer>
+                <p>
+                    Блог<br>Copyright &copy; 2016
+                </p>
+            </footer>
+        </div>
+    </body>
+</hmtl>
